83fe3bcf6a16f00ab76e2d1116ff373027035797
Saideep Kankarla <saideep.kankarla@gmail.com>
2018-10-25 00:58:46 +0530

    MCA6-7 : Video Upload Controller and DB changes

--- routes/signRoutes.js
+++ routes/signRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/db');
+const db = require('../config/db');
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('./config/db');
-const sign = require('./signRoutes.js');
+const sign = require('./routes/signRoutes.js');
+const multer = require('multer');
 const app = express();
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/videos/1')
+    },
+    filename: (req, file, cb) => {
+        fileExtension = file.originalname.split('.')[1] // get file extension from original file name
+        cb(null, file.fieldname + '-' + Date.now() + '.' + fileExtension)
+    }
+});
+var upload = multer({ storage: storage });
 
 //middlewares
 app.use(express.static(path.join(__dirname)));
@@ -11,24 +23,53 @@ app.use(express.static(path.join(__dirname)));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-console.log(__dirname);
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 //Categorizing Routes
 app.use('/sign', sign.myRouter);
 
-
 app.get('/', function(req, res) {
-    db.query('SELECT * FROM adminlogin', function(err, result) {
+    res.render('pages/index');
+});
+
+app.get('/upload', function(req, res) {
+    res.render('pages/upload');
+});
+
+app.post('/uploadProcess', upload.single('videoFile'), function(req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    const userId = 1;
+    const videoTitle = req.body.video_title;
+    const videoDesc = req.body.video_description;
+    const videoDuration = "5:08";
+    const category = req.body.category;
+    const fileUploadedPath = req.file.filename;
+    const fileUploadedOn = new Date().toLocaleString();
+    const isPrivate = req.body.videoPrivate;
+    let assignPrivate;
+    //If Private
+    if (isPrivate === 'on') {
+        assignPrivate = 0;
+    } else {
+        assignPrivate = 1;
+    }
+
+    let insQuery = "INSERT INTO `videos` (user_id, category_id, video_name, video_description, video_duration,video_upload_path, video_uploadedon, isPublic, status) VALUES ('" +
+        userId + "', '" + category + "','" + videoTitle + "', '" + videoDesc + "', '" + videoDuration + "', '" + fileUploadedPath + "', '" + fileUploadedOn + "',  '" + assignPrivate + "', '" + "Active" + "')";
+    console.log(insQuery);
+    db.query(insQuery, function(err, result) {
         if (err) {
             throw err;
         } else {
             //res.render('pages/index', { data: result });
-            res.render('pages/index', { dataMsg: "" });
+            res.redirect('/upload');
         }
     });
-});
+
+})
+
 
 
 app.listen(3000);
