abd5ca451ba8d1aec98b03b1c054995bd2492164
CSG <skankarla@ciphercloud.com>
2018-10-28 01:53:21 +0530

    MCA6-11 : update sql file

--- videoapp.sql
+++ videoapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2018 at 09:49 PM
+-- Generation Time: Oct 27, 2018 at 10:22 PM
 -- Server version: 5.5.39
 -- PHP Version: 5.4.31
 
@@ -51,7 +51,18 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `name` varchar(80) NOT NULL,
   `description` text NOT NULL,
   `status` varchar(7) NOT NULL DEFAULT 'Active'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `name`, `description`, `status`) VALUES
+(1, 'Arts', 'Arts videos comes here', 'Active'),
+(2, 'Education', 'Education videos comes here', 'Active'),
+(3, 'Entertainment', 'Entertainment videos comes here', 'Active'),
+(4, 'Science', 'Science videos comes here', 'Active'),
+(5, 'Technology', 'Technology videos comes here', 'Active');
 
 -- --------------------------------------------------------
 
@@ -81,7 +92,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email_id` varchar(200) NOT NULL,
   `password` varchar(20) NOT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'Active'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `phone`, `email_id`, `password`, `status`) VALUES
+(1, 'saideep', 1234567890, 'saideep.kankarla@gmail.com', '123456', 'Active'),
+(2, 'test', 1234567890, 'test.kankarla@gmail.com', '12345678', 'Active'),
+(3, 'IDeep', 1234567890, 'deep.kankarla@gmail.com', '123456', 'Active');
 
 -- --------------------------------------------------------
 
@@ -95,12 +115,21 @@ CREATE TABLE IF NOT EXISTS `videos` (
   `category_id` int(11) NOT NULL,
   `video_name` varchar(255) NOT NULL,
   `video_description` text NOT NULL,
-  `video_duration` int(10) NOT NULL,
+  `video_duration` varchar(10) NOT NULL,
   `video_upload_path` text NOT NULL,
   `video_uploadedon` varchar(30) NOT NULL,
   `isPublic` varchar(5) NOT NULL DEFAULT 'true',
   `status` varchar(10) NOT NULL DEFAULT 'Active'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `videos`
+--
+
+INSERT INTO `videos` (`video_id`, `user_id`, `category_id`, `video_name`, `video_description`, `video_duration`, `video_upload_path`, `video_uploadedon`, `isPublic`, `status`) VALUES
+(1, 2, 2, 'Test Video', 'some Bla', '0:00', 'videoFile-1540666662047.js', '2018-10-28 00:27:42', 'false', 'Active'),
+(2, 2, 2, 'My video 2', 'asasa', '0:00', 'videoFile-1540666826650.avi', '2018-10-28 00:30:27', 'true', 'Active'),
+(3, 1, 1, 'My video 1', 'qwqwq', '0:00', 'videoFile-1540667389546.mp4', '2018-10-28 00:39:49', 'false', 'Active');
 
 -- --------------------------------------------------------
 
@@ -168,7 +197,7 @@ MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `comments`
 --
@@ -178,12 +207,12 @@ MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `videos`
 --
 ALTER TABLE `videos`
-MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `videos_history`
 --
