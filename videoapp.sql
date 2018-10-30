6786875ceaceabdf21746305ea670c5ba7764f52
Saideep Kankarla <saideep.kankarla@gmail.com>
2018-10-31 01:43:59 +0530

    MCA6-21 : css fix for dashboard

--- videoapp.sql
+++ videoapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2018 at 05:15 PM
+-- Generation Time: Oct 30, 2018 at 09:12 PM
 -- Server version: 5.5.39
 -- PHP Version: 5.4.31
 
@@ -78,16 +78,14 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `phone` int(10) NOT NULL,
   `comments` text NOT NULL,
   `status` varchar(8) NOT NULL DEFAULT 'Active'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `comments`
 --
 
 INSERT INTO `comments` (`comment_id`, `video_id`, `name`, `email`, `phone`, `comments`, `status`) VALUES
-(1, 1, 'Saideep', 'saideep.kankarla@gmail.com', 123456, 'Test comments', 'Active'),
-(2, 1, 'Saideep1', 'saideep1.kankarla@gmail.com', 123456, 'A great video', 'Active'),
-(3, 2, 'Dev Man', 'devman@gmail.com', 789452136, 'A great cartton video', 'Active');
+(1, 1, 'Saideep', 'saideep.kankarla@gmail.com', 2147483647, 'Adding my 1st comment', 'Active');
 
 -- --------------------------------------------------------
 
@@ -102,15 +100,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email_id` varchar(200) NOT NULL,
   `password` varchar(20) NOT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'Active'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `name`, `phone`, `email_id`, `password`, `status`) VALUES
-(1, 'saideep', 1234567890, 'skankarla@gmail.com', '123456', 'Active'),
-(2, 'karthik', 1234567890, 'kkankarla@gmail.com', '123456', 'Active');
+(1, 'saideep', 1234567890, 'skankarla@gmail.com', '12345678', 'Active');
 
 -- --------------------------------------------------------
 
@@ -131,15 +128,21 @@ CREATE TABLE IF NOT EXISTS `videos` (
   `video_views` int(10) NOT NULL DEFAULT '0',
   `isPublic` varchar(5) NOT NULL DEFAULT 'true',
   `status` varchar(10) NOT NULL DEFAULT 'Active'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `videos`
 --
 
 INSERT INTO `videos` (`video_id`, `user_id`, `category_id`, `video_name`, `video_description`, `video_thumbnail`, `video_duration`, `video_upload_path`, `video_uploadedon`, `video_views`, `isPublic`, `status`) VALUES
-(1, 1, 2, 'My video 1', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia', 'videoThumb-1540758789186.png', '', 'videoFile-1540758789194.mp4', '2018-10-29 02:03:09', 44, 'true', 'Active'),
-(2, 2, 4, 'Lion video', 'Lions have strong, compact bodies and powerful forelegs, teeth and jaws for pulling down and killing prey. Their coats are yellow-gold, and adult males have shaggy manes that range in color from blond to reddish-brown to black.\r\n\r\nThe darker and thicker the mane, the healthier the lion. It allows the lions to appear stronger and frightening to warn off enemies, particularly humans, and appeals to lionesses that are scientifically proven to mate more with lions with thick and dark manes.', 'videoThumb-1540814839747.png', '', 'videoFile-1540814839783.mp4', '2018-10-29 17:37:20', 5, 'true', 'Active');
+(1, 1, 2, 'My video 1', 'Some desc comes here', 'videoThumb-1540908579790.jpg', '', 'videoFile-1540908579791.mp4', '10/30/2018, 7:39:39 PM', 2, 'true', 'Active'),
+(2, 1, 2, 'Cloud functions', 'My video 2', 'videoThumb-1540908704081.jpg', '', 'videoFile-1540908704086.mp4', '10/30/2018, 7:41:44 PM', 0, 'true', 'Active'),
+(3, 1, 3, 'React basics', 'React basics desc', 'videoThumb-1540908733114.jpg', '', 'videoFile-1540908733116.mp4', '10/30/2018, 7:42:13 PM', 0, 'true', 'Active'),
+(4, 1, 5, 'New AWS Series', 'New AWS Series desc comes here', 'videoThumb-1540908770944.jpg', '', 'videoFile-1540908770944.mp4', '10/30/2018, 7:42:50 PM', 0, 'true', 'Active'),
+(5, 1, 2, 'Node js basics', 'Node js basics desc comes here', 'videoThumb-1540908869201.jpg', '', 'videoFile-1540908869203.mp4', '10/30/2018, 7:44:29 PM', 0, 'true', 'Active'),
+(6, 1, 4, 'PHP Storm', 'PHP Storm desc comes here', 'videoThumb-1540908904639.jpg', '', 'videoFile-1540908904640.mp4', '10/30/2018, 7:45:04 PM', 0, 'true', 'Active'),
+(7, 1, 1, 'CSS 3 Tutorials', 'CSS 3 Tutorials explained', 'videoThumb-1540908979143.jpg', '', 'videoFile-1540908979146.mp4', '10/30/2018, 7:46:19 PM', 0, 'true', 'Active'),
+(8, 1, 5, 'Git Basics', 'Git Basics come shere', 'videoThumb-1540909014237.jpg', '', 'videoFile-1540909014239.mp4', '10/30/2018, 7:46:54 PM', 0, 'true', 'Active');
 
 -- --------------------------------------------------------
 
@@ -211,17 +214,17 @@ MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `videos`
 --
 ALTER TABLE `videos`
-MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `videos_history`
 --
